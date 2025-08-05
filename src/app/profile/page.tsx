9d453ab3c6f8b5dcd3746e744973eb3f1921f3e3
Mark Protsyuk <protsyukmark@gmail.com>
2025-08-05 16:40:25 -0700

    learned the basics of routes in nextJS

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+export default function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>This is the profile page of our application.</p>
+      <p>Here you can view and edit your profile information.</p>
+      <p>Feel free to customize your profile settings.</p>
+      <p>Remember to save any changes you make!</p>
+    </div>
+  );
+}
